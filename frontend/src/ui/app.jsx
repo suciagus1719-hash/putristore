b751a16a577ce9c618f97dc0d44f25696d5343b4
suciagus1719-hash <suciagus1719@gmail.com>
2025-11-04 19:55:32 +0800

    fix: path import App & LuxuryOrderFlow, set HashRouter

--- frontend/src/ui/app.jsx
+++ frontend/src/ui/app.jsx
@@ -1,8 +1,9 @@
-import LuxuryOrderFlow from "../LuxuryOrderFlow";
-
+import LuxuryOrderFlow from "../LuxuryOrderFlow.jsx"; // naik 1 folder dari /ui ke /src
 
 export default function App() {
   return (
-    <LuxuryOrderFlow apiBase={import.meta.env.VITE_API_BASE || "http://localhost:3001"} />
+    <LuxuryOrderFlow
+      apiBase={import.meta.env.VITE_API_BASE || "http://localhost:3001"}
+    />
   );
 }

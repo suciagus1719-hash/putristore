4c0ac0ddc27231782572166723a5eb5d217e4900
suciagus1719-hash <suciagus1719@gmail.com>
2025-11-12 00:56:54 +0800

    Set homepage to new LuxuryOrderFlow

--- frontend/src/ui/app.jsx
+++ frontend/src/ui/app.jsx
@@ -9,11 +9,11 @@ const API_BASE = import.meta.env.VITE_API_URL || "https://putristore-backend.ver
 
 export default function App() {
   return (
-   <Routes>
-  <Route path="/" element={<PaymentFlow />} />
-  <Route path="/classic" element={<LuxuryOrderFlow apiBase={API_BASE} />} />
-  <Route path="/admin" element={<AdminPanel />} />
-</Routes>
+    <Routes>
+      <Route path="/" element={<LuxuryOrderFlow apiBase={API_BASE} />} />
+      <Route path="/payment" element={<PaymentFlow />} />
+      <Route path="/admin" element={<AdminPanel />} />
+    </Routes>
 
   );
 }

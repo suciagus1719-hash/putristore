55852fc34b40d8d5cae7960706f2c6996c6f078e
suciagus1719-hash <suciagus1719@gmail.com>
2025-11-11 22:47:50 +0800

    Deploy payment flow

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,7 @@
-console.log("MAIN_JSX_LOADED");
-
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { HashRouter } from "react-router-dom";
-import App from "./ui/app.jsx";   // <- perhatikan huruf kecil dan path
+import App from "./ui/app.jsx";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>

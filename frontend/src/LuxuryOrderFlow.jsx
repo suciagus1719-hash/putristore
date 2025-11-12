a26fae8e1c01023372165e13c243c82b4537fa45
suciagus1719-hash <suciagus1719@gmail.com>
2025-11-12 23:51:30 +0800

    Percepat pemilihan layanan & update CORS admin

--- frontend/src/LuxuryOrderFlow.jsx
+++ frontend/src/LuxuryOrderFlow.jsx
@@ -1,4 +1,4 @@
-import\ React,\ \{\ useCallback,\ useEffect,\ useMemo,\ useRef,\ useState\ }\ from\ "react";
+import React, { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import {
   Instagram,

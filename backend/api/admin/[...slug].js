990cd6fb28336a36d075d58564c5d7e1f86bcc3d
suciagus1719-hash <suciagus1719@gmail.com>
2025-11-15 11:17:11 +0800

    fix: respond to admin CORS preflight

--- backend/api/admin/[...slug].js
+++ backend/api/admin/[...slug].js
@@ -1,20 +1,4 @@
 const handler = require("./index");
 
-function sendCorsOptions(res) {
-  const origin = process.env.FRONTEND_ORIGIN || "https://suciagus1719-hash.github.io";
-  res.setHeader("Access-Control-Allow-Origin", origin);
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PATCH,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Accept, Authorization, X-Admin-Key");
-  res.setHeader("Access-Control-Max-Age", "86400");
-  res.status(204).end();
-}
-
-const wrapped = (req, res) => {
-  if (req.method === "OPTIONS") {
-    return sendCorsOptions(res);
-  }
-  return handler(req, res);
-};
-
-module.exports = wrapped;
-module.exports.handler = wrapped;
+module.exports = (req, res) => handler(req, res);
+module.exports.handler = handler;
